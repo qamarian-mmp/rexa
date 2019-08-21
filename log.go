8208337ae7d92870113ebb80ba6f5552e93525ac
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-21 17:58:06 +0100

    Progress 3

---
+++ log.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"gopkg.in/qamarian-lib/str.v2"
+	"gopkg.in/qamarian-mmp/rxlib.v1"
+)
+
+// This data type is an emissary of the abstract data type rxlib.RxLog.
+type log struct {}
+
+func (l *log) Record (newLog string, logType byte) (error) {
+	outputType := "std"
+	switch logType {
+		case rxlib.LogWarning: outputType = "wrn"
+		case rxlib.LogError: outputType = "err"
+	}
+	str.PrintEtr (newLog, outputType, "rexa")
+	return nil
+}
