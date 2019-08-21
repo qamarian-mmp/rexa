94f02cd775756ff15434cafc64ef3c0bd7544855
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-21 18:08:22 +0100

    Progress 4

--- log.go
+++ log.go
@@ -5,6 +5,11 @@ import (
 	"gopkg.in/qamarian-mmp/rxlib.v1"
 )
 
+// NewLog () helps create a new log.
+func NewLog () (*log) {
+	return &log {}
+}
+
 // This data type is an emissary of the abstract data type rxlib.RxLog.
 type log struct {}
 
